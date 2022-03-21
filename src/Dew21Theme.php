a93acbde76c10561611e7c8d0ccca7ce8ad94966
Andreas Mueck <a.mueck@freenet.de>
2022-03-21 15:20:20 +0100

    Dew21Theme Initial Commit

---
+++ src/Dew21Theme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Dew21Theme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Dew21Theme extends Plugin implements ThemeInterface
+{
+}
